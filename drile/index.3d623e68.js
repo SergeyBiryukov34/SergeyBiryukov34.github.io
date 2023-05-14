b87a7bd36c3f5b71fc95534866d99285dd4ad846
SergeyBiryukov <sergeybiryukov34@gmail.com>
2023-05-14 10:07:21 +0300

    added drile

---
+++ drile/index.3d623e68.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",(()=>{const e=document.querySelector("body"),t=document.querySelector(".header-actions__burger"),o=document.querySelector(".header-nav");e.classList.remove("loader"),t.addEventListener("click",(t=>{t.preventDefault(),o.classList.toggle("header-nav_open"),e.classList.toggle("overlay")}))}));
+//# sourceMappingURL=index.3d623e68.js.map
